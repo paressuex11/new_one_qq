01d3f3aeacf60c4fba1d49120fd88d098b659f4a
paressuex11 <1138514846@qq.com>
2019-04-10 23:11:12 +0800

    0

--- networks/networks.py
+++ networks/networks.py
@@ -15,14 +15,22 @@ def server(port):
         data = text.encode("ascii")
         sock.sendto(data, address)
 
+
 def client(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     text = "The time is {}".format(datetime.now())
     data = text.encode("ascii")
     print("The OS assigned me the address {}".format(sock.getsockname()))
     data, address = sock.recvfrom(Max_bytes)
+    text = data.encode("ascii")
+    print("The server {} replied {!r}".format(address, text))
 
 
-
-
-
+if __name__ == "__main__":
+    choices = {'client':client, 'server':server}
+    parser = argparse.ArgumentParser(description = "Send and receive UDP locally")
+    parser.add_argument('role', choices = choices, help = 'which role to play')
+    parser.add_argument('-p', metavar = 'PORT', type = int, default = 1060, help = "UDP port (default 1060)")
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.p)
