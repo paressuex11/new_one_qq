b0918786b8e53d2d9187681fb45f74c21ee3db5a
paressuex11 <1138514846@qq.com>
2019-04-10 23:00:51 +0800

    0

--- networks/networks.py
+++ networks/networks.py
@@ -1,1 +1,28 @@
+import socket, argparse
+from datetime import datetime
+
+Max_bytes = 65535
+
+def server(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("localhost", port))
+    print("Listening at {}".format(sock.getsockname))
+    while True:
+        data, address = sock.recvfrom(Max_bytes)
+        text = data.decode("ascii")
+        print("The client at {} says {!r}".format(address, text))
+        text = "Your data was {} bytes long".format(len(data))
+        data = text.encode("ascii")
+        sock.sendto(data, address)
+
+def client(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    text = "The time is {}".format(datetime.now())
+    data = text.encode("ascii")
+    print("The OS assigned me the address {}".format(sock.getsockname()))
+    data, address = sock.recvfrom(Max_bytes)
+
+
+
+
 
